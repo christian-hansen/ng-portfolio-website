ac94f0a384b6562e00ec240711fe6770878f43f3
Christian Hansen <68639582+christian-hansen@users.noreply.github.com>
2023-06-06 16:07:30 +0200

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { SectionTitleComponent } from './section-title/section-title.component';
+import { SectionAboutComponent } from './section-about/section-about.component';
+import { SectionSkillsComponent } from './section-skills/section-skills.component';
+import { SectionPortfolioComponent } from './section-portfolio/section-portfolio.component';
+import { SectionContactComponent } from './section-contact/section-contact.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SectionTitleComponent,
+    SectionAboutComponent,
+    SectionSkillsComponent,
+    SectionPortfolioComponent,
+    SectionContactComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
