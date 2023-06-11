e12dfabddc0ad90098711437e9e02b578767c8f0
Christian Hansen <68639582+christian-hansen@users.noreply.github.com>
2023-06-11 16:29:28 +0200

    portfolio section

--- src/app/section-portfolio/section-portfolio.component.ts
+++ src/app/section-portfolio/section-portfolio.component.ts
@@ -7,8 +7,18 @@ import { Component } from '@angular/core';
 })
 export class SectionPortfolioComponent {
   displayPortfolioItem1 = true;
+  displayPortfolioItem2 = true;
+  displayPortfolioItem3 = true;
+  displayPortfolioItem4 = true;
+  displayPortfolioItem5 = true;
 
-  togglePortfolioItem() {
-    this.displayPortfolioItem1 = !this.displayPortfolioItem1;
+  togglePortfolioItem(input: number) {
+    console.log(input);
+    
+  if (input === 1) {this.displayPortfolioItem1 = !this.displayPortfolioItem1;}
+  else if (input === 2) {this.displayPortfolioItem2 = !this.displayPortfolioItem2;}
+  else if (input === 3) {this.displayPortfolioItem3 = !this.displayPortfolioItem3;}
+  else if (input === 4) {this.displayPortfolioItem4 = !this.displayPortfolioItem4;}
+  else if (input === 5) {this.displayPortfolioItem5 = !this.displayPortfolioItem5;}
   }
 }
