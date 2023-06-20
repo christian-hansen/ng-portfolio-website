b20ac0137531a9204cbb648b220a92b7fb2ef68c
Christian Hansen <68639582+christian-hansen@users.noreply.github.com>
2023-06-20 13:11:01 +0200

    contact form functions

--- src/app/contact-form/contact-form.component.ts
+++ src/app/contact-form/contact-form.component.ts
@@ -1,11 +1,56 @@
-import { Component, ViewChild, ElementRef, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-contact-form',
   templateUrl: './contact-form.component.html',
   styleUrls: ['./contact-form.component.scss'],
 })
-export class ContactFormComponent{
-  inputData = {inputName: '', inputEmail: '', inputMessage: ''}
+export class ContactFormComponent {
+  inputData = { inputName: '', inputEmail: '', inputMessage: '' };
+
+  async sendMail() {
+    //Animation anzeigen
+
+    let formData = new FormData();
+    this.appendFormData(formData, this.inputData);
+
+    //senden
+
+    await fetch(
+      'https://christian-hansen.developerakademie.net/testmail/send_mail/send_mail.php',
+      {
+        method: 'POST',
+        body: formData,
+      }
+    );
+
+    //Text anzeigen "Nachricht gesendet"
+  }
+
+  appendFormDatas(
+    formData: FormData,
+    nameField: string,
+    emailField: string,
+    messageField: string
+  ) {
+    formData.append('name', nameField);
+    formData.append('email', emailField);
+    formData.append('message', messageField);
+  }
+
+  appendFormData(formData: FormData, inputData: any) {
+    formData.append('name', inputData.inputName);
+    formData.append('email', inputData.inputEmail);
+    formData.append('message', inputData.inputMessage);
+  }
+
+  logMail() {
+    console.log(
+      'Message to send:','\n',
+      this.inputData.inputName,'\n',
+      this.inputData.inputEmail,'\n',
+      this.inputData.inputMessage
+    );
+    // this.sendMail();
+  }
 }
