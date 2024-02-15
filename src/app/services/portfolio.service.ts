a4a48aaf124364542c86e5366c99d030442e135b
Christian Hansen <68639582+christian-hansen@users.noreply.github.com>
2024-02-15 21:06:48 +0100

    update portfolio

--- src/app/services/portfolio.service.ts
+++ src/app/services/portfolio.service.ts
@@ -16,9 +16,18 @@ portfolioitems: any = [
     tags: ['Angular', 'Typescript', 'HTML', 'CSS', 'Firebase']
   },
   {
+    mainImage: "/assets/img/portfolio/react-store-main.png",
+    backgroundImage: "/assets/img/portfolio/react-store-bg.png",
+    gitLink: 'https://github.com/christian-hansen/react-mini-online-store',
+    liveLink: 'https://react-store.christian-hansen.dev/',
+    title: 'React Store',
+    description: 'A dynamic online store built with React and Bootstrap, showcasing a responsive design, intuitive user interface, and seamless shopping experience.',
+    tags: ['React', 'Javascript', 'HTML', 'CSS', 'Bootstrap']
+  },
+  {
     mainImage: "/assets/img/portfolio/Join01.png",
     backgroundImage: "/assets/img/portfolio/Join00.png",
-    gitLink: 'https://github.com/christian-hansen/join-app',
+    gitLink: 'https://github.com/christian-hansen/js-join-app',
     liveLink: 'https://christian-hansen.developerakademie.net/join/',
     title: 'JOIN',
     description: 'Explore "JOIN" a user-friendly Kanban board and Customer Relationship Management (CRM) app built with vanilla Javascript.',
@@ -27,7 +36,7 @@ portfolioitems: any = [
   {
     mainImage: "/assets/img/portfolio/Pollo-Loco01.png",
     backgroundImage: "/assets/img/portfolio/Pollo-Loco00.png",
-    gitLink: 'https://github.com/christian-hansen/el-pollo-loco',
+    gitLink: 'https://github.com/christian-hansen/js-el-pollo-loco',
     liveLink: 'https://christian-hansen.developerakademie.net/elpolloloco/',
     title: 'El Pollo Loco',
     description: 'Embark on an adventure with "El Pollo Loco", a captivating jump and run game crafted using object-oriented JavaScript.',
@@ -36,7 +45,7 @@ portfolioitems: any = [
   {
     mainImage: "/assets/img/portfolio/Website01.png",
     backgroundImage: "/assets/img/portfolio/Website00.jpeg",
-    gitLink: 'https://github.com/christian-hansen/portfolio-website',
+    gitLink: 'https://github.com/christian-hansen/ng-portfolio-website',
     liveLink: 'https://www.christian-hansen.dev',
     title: 'Personal website',
     description: 'Discover my personal portfolio website, crafted using the Angular framework for a sleek and modern design.',
